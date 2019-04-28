e75dcffe9e8b75fc1037e3a6a6d51dd49a752e7a
harshablast <harshablast@gmail.com>
2019-04-28 06:37:19 +0530

    Added drawing functionality and socket thread

--- videowindow.py
+++ videowindow.py
@@ -1,5 +1,5 @@
 from tkinter import Label, Tk, Frame
-from ttkthemes import themed_tk as tk
+# from ttkthemes import themed_tk as tk
 import threading
 import cv2
 from PIL import Image
@@ -7,14 +7,15 @@ from PIL import ImageTk
 import imutils
 import socket
 
-s = socket.socket()
+# s = socket.socket()
 
-s.bind(('192.168.43.212', 8090))
-s.listen(0)
+# s.bind(('192.168.43.212', 8090))
+# s.listen(0)
 
 
 class videofeed:
     def __init__(self, vs, outputPath):
+
         self.vs = vs
         self.outputPath = outputPath
         self.frame = None
@@ -25,9 +26,22 @@ class videofeed:
         self.panel2 = None
         self.stopEvent = threading.Event()
         self.thread = threading.Thread(target=self.videoLoop, args=())
+        self.thread2 = threading.Thread(target=self.penLoop, args = ())
         self.thread.start()
+        self.thread2.start()
         self.root.wm_title("LiveEd")
         self.root.wm_protocol("WM_DELETE_WINDOW", self.onClose)
+
+        self.drawings = []
+        self.pen_properties = {
+            color: (255, 0, 0)
+            size: 10
+        }
+        self.pen_state = 0
+        self.drawing_state = False
+        self.pen_location = (0, 0)
+
+        self.query_images = []
 
     def onClose(self):
         # set the stop event, cleanup the camera, and allow the rest of
@@ -37,12 +51,14 @@ class videofeed:
         self.vs.release()
         self.root.quit()
 
-    def getData(self):
+    def penLoop(self):
+        
         while True:
 
             client, addr = s.accept()
 
             while True:
+                
                 content = client.recv(32)
                 arduinoData = content.decode('ascii').strip()
 
@@ -50,7 +66,7 @@ class videofeed:
                     break
 
                 else:
-                    return arduinoData
+                    self.pen_state = arduinoData
 
             print("Closing connection")
             client.close()
@@ -60,55 +76,65 @@ class videofeed:
 
             while not self.stopEvent.is_set():
                 _, self.frame = self.vs.read()
-                self.frame = imutils.resize(self.frame, width=1420)
+                self.frame = imutils.resize(self.frame, width=800)
 
                 image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
                 image = cv2.flip(image, 1)
-                image = Image.fromarray(image)
-                image = ImageTk.PhotoImage(image)
+
+                self.pen_location = get_pen_location(image)
+
+                if self.pen_state == 1:
+
+                    if self.drawing_state:
+
+                        drawings[-1].append(pen_location)
+
+                    else:
+
+                        drawings.append([])
+                        drawings[-1].append(pen_location)
+
+                else:
+
+                    if self.drawing_state:
+
+                        self.drawing_state = False
+
+
+
+                tkinter_image = Image.fromarray(image)
+                tkinter_image = ImageTk.PhotoImage(tkinter_image)
 
                 # if the panel is None, we need to initialize it
                 if self.panel is None:
-                    self.panel = Label(image=image)
-                    self.panel.image = image
+                    self.panel = Label(image=tkinter_image)
+                    self.panel.image = tkinter_image
                     self.panel.pack(side="left", padx=10, pady=10)
-                    self.panel2 = Frame(self.root, height="1080", width="500", bg="red")
+                    self.panel2 = Frame(self.root, height="1080", width="300", bg="red")
                     self.panel2.pack(side="left", padx=10, pady=10)
                     Label(self.panel2, text='Sir, I dont understand why you use Tkinter', borderwidth=1).pack(
                         side="top")
-                    Label(self.panel2, text=self.getData(), borderwidth=1).pack(side="top")
+                    Label(self.panel2, text="lol2", borderwidth=1).pack(side="top")
                 # otherwise, simply update the panel
                 else:
-                    self.panel.configure(image=image)
-                    self.panel.image = image
+                    self.panel.configure(image=tkinter_image)
+                    self.panel.image = tkinter_image
 
         except RuntimeError:
             print("[INFO] caught a RuntimeError")
 
+    def process_image(self, image):
 
-vs = cv2.VideoCapture(0)
+        blank_image = np.zeros(h_img, w_img, 3, dtype = np.uint8)
 
-pba = videofeed(vs, r"D:/")
-
-# comments shit
+        for drawing in drawings:
 
 
-pba.root.mainloop()
 
-# root = tk.ThemedTk()
-# root.get_themes()
-# root.set_theme("radiance")
-#
-# #video
-# videoframe = Frame(root, height=1080, width=1353, bg="white")
-#
-#
-# #chat
-# chatframe = Frame(root, height = 1080, width=567,bg="gray")
-#
-#
-# #packing
-# videoframe.pack(side=LEFT)
-# chatframe.pack(side=LEFT)
-#
-# root.mainloop()
+
+if __name__ == "__main__":
+
+    vs = cv2.VideoCapture(0)
+    pba = videofeed(vs, r"D:/")
+
+    pba.root.mainloop()
