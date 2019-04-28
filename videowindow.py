beea5b2ba87bebc948b729ded6d83deec7abebb4
KillerBreadMan <ankushmalaker2000@gmail.com>
2019-04-28 12:36:30 +0530

    almost everything working

--- videowindow.py
+++ videowindow.py
@@ -1,6 +1,7 @@
 from tkinter import Label, Tk, Frame
 # from ttkthemes import themed_tk as tk
 import threading
+import numpy as np
 import cv2
 from PIL import Image
 from PIL import ImageTk
@@ -14,8 +15,8 @@ import shutil
 from google_images_search import GoogleImagesSearch
 
 s = socket.socket()
-
 s.bind(('192.168.43.115', 8090))
+
 s.listen(0)
 r = sr.Recognizer()
 mic = sr.Microphone(device_index=5)
@@ -24,16 +25,16 @@ CHANNELS = 2
 RATE = 44100
 CHUNK = 1024
 RECORD_SECONDS = 2
-API_KEY= 'AIzaSyCki8KIN6wWL06X5bvbVVykYBrLgQoMofw'
+API_KEY = 'AIzaSyCki8KIN6wWL06X5bvbVVykYBrLgQoMofw'
 CX = '009518592146397428949:hm3iqr4gpv4'
 gis = GoogleImagesSearch(API_KEY, CX)
 WAVE_OUTPUT_FILENAME = "recorded_audio.wav"
 frames = []
 
 query_images = {
-        'images': [],
-        'coords': []
-    }
+    'images': [],
+    'coords': []
+}
 
 
 class videofeed:
@@ -49,7 +50,7 @@ class videofeed:
         self.panel2 = None
         self.stopEvent = threading.Event()
         self.thread = threading.Thread(target=self.videoLoop, args=())
-        self.thread2 = threading.Thread(target=self.penLoop, args = ())
+        self.thread2 = threading.Thread(target=self.penLoop, args=())
         self.thread3 = threading.Thread(target=self.get_image, args=())
         self.thread.start()
         self.thread2.start()
@@ -65,7 +66,7 @@ class videofeed:
         self.drawing_state = False
         self.pen_location = (0, 0)
         self.query_image_coord = (0, 0)
-        self.query_image = None
+        self.query_image = ''
         self.get_image_flag = False
 
     def get_image(self):
@@ -122,13 +123,13 @@ class videofeed:
         self.root.quit()
 
     def penLoop(self):
-        
+
         while True:
 
             client, addr = s.accept()
 
             while True:
-                
+
                 content = client.recv(32)
                 arduinoData = content.decode('ascii').strip()
 
@@ -148,46 +149,46 @@ class videofeed:
                 _, self.frame = self.vs.read()
                 self.frame = imutils.resize(self.frame, width=800)
 
+                self.frame = cv2.flip(self.frame, 1)
+
+                self.pen_location = self.get_pen_location(self.frame)
+
                 image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
-                image = cv2.flip(image, 1)
+                # image = cv2.flip(image, 1)
 
-                self.pen_location = get_pen_location(image)
+                if self.pen_state == '1':
 
-                if self.pen_state == 1:
+                    print('elfaks')
 
                     if self.drawing_state:
 
-                        drawings[-1].append(pen_location)
+                        self.drawings[-1].append(self.pen_location)
 
                     else:
 
-                        drawings.append([])
-                        drawings[-1].append(pen_location)
+                        self.drawing_state = True
+                        self.drawings.append([])
+                        self.drawings[-1].append(self.pen_location)
 
                 else:
 
                     if self.drawing_state:
-
                         self.drawing_state = False
 
-                if self.pen_state == 2:
+                if self.pen_state == '2':
 
-                    if (!self.get_image_flag):
-
+                    if (self.get_image_flag == False):
                         self.get_image_flag = True
                         self.query_image_coord = self.pen_location
-                        self.query_image = []
+                        self.query_image = ''
                         self.thread3.start()
 
-                if(self.get_image_flag):
+                if (self.get_image_flag):
 
-                    if query_image != None:
+                    if self.query_image != '':
+                        self.get_image_flag = False
 
-                        self.get_image_flag == False
-
-
-
-
+                image = self.process_image(image)
 
                 tkinter_image = Image.fromarray(image)
                 tkinter_image = ImageTk.PhotoImage(tkinter_image)
@@ -214,57 +215,119 @@ class videofeed:
 
     def process_image(self, image):
 
-        for drawing in drawings:
+        for drawing in self.drawings:
 
-            for i in range(len(drawing)-1):
+            for i in range(len(drawing) - 1):
+                cv2.line(image, drawing[i], drawing[i + 1], (255, 0, 0), 2)
 
-                cv2.line(image, drawing[i], drawing[i+1], (255, 0, 0), 2)
-
-        if self.query_image != None:
-
+        if self.query_image != '':
+            print(self.query_image)
             q_img = cv2.imread(self.query_image)
             q_img = cv2.resize(q_img, (150, 150))
 
-            image[self.query_image_coord[1]:self.query_image_coord[1] + 150, self.query_image_coord[0]:self.query_image_coord[0] + 150] = q_img
+            image[self.query_image_coord[1]:self.query_image_coord[1] + 150,
+            self.query_image_coord[0]:self.query_image_coord[0] + 150] = q_img
 
+        cv2.circle(image, self.pen_location, 2, (0, 255, 0), -1)
+
+        return image
 
     def get_pen_location(self, img):
 
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    
-        lower_1 = np.array([0, 70, 170], dtype = np.uint8)
-        upper_1 = np.array([20, 130, 230], dtype = np.uint8)
+        kernel_size = 5
+        kernel1 = np.ones((kernel_size, kernel_size), np.float32) / kernel_size / kernel_size
 
-        lower_2 = np.array([160, 70, 170], dtype = np.uint8)
-        upper_2 = np.array([190, 130, 230], dtype = np.uint8)
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+        lower_1 = np.array([0, 120, 130], dtype=np.uint8)
+        upper_1 = np.array([10, 200, 255], dtype=np.uint8)
+
+        lower_2 = np.array([170, 120, 130], dtype=np.uint8)
+        upper_2 = np.array([180, 200, 255], dtype=np.uint8)
 
         mask_1 = cv2.inRange(hsv, lower_1, upper_1)
         mask_2 = cv2.inRange(hsv, lower_2, upper_2)
 
         mask = mask_1 + mask_2
-        
 
-        cv2.imshow('Mask', mask)
+        res = cv2.bitwise_and(hsv, hsv, mask=mask)
+        res = cv2.erode(res, kernel1, iterations=1)
+        res = cv2.dilate(res, kernel1, iterations=1)
+
+        rgb = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
+
+        gray = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+
+        gray = cv2.GaussianBlur(gray, (11, 11), 0)
+        ret, gray = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
 
         white_coords = np.argwhere(mask == 255)
-
 
         if white_coords.shape == (0, 2):
             topmost_last = (360, 512)
         else:
-            topmost_last = np.average(white_coords, axis = 0)
+            topmost_last = np.average(white_coords, axis=0)
             topmost_last = topmost_last.astype(np.int)
             topmost_last[0], topmost_last[1] = topmost_last[1], topmost_last[0]
             topmost_last = tuple(topmost_last)
-        
+
         return topmost_last
 
 
-
-
 if __name__ == "__main__":
-
     vs = cv2.VideoCapture(0)
     pba = videofeed(vs, r"D:/")
 
     pba.root.mainloop()
+
+    # while(1):
+    #
+    #     _, img = vs.read()
+    #
+    #     kernel_size = 5
+    #     kernel1 = np.ones((kernel_size, kernel_size), np.float32) / kernel_size / kernel_size
+    #
+    #     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    #
+    #     lower_1 = np.array([0, 120, 130], dtype=np.uint8)
+    #     upper_1 = np.array([10, 200, 255], dtype=np.uint8)
+    #
+    #     lower_2 = np.array([170, 120, 130], dtype=np.uint8)
+    #     upper_2 = np.array([180, 200, 255], dtype=np.uint8)
+    #
+    #     mask_1 = cv2.inRange(hsv, lower_1, upper_1)
+    #     mask_2 = cv2.inRange(hsv, lower_2, upper_2)
+    #
+    #     mask = mask_1 + mask_2
+    #
+    #     res = cv2.bitwise_and(hsv, hsv, mask=mask)
+    #     res = cv2.erode(res, kernel1, iterations=1)
+    #     res = cv2.dilate(res, kernel1, iterations=1)
+    #
+    #     rgb = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
+    #
+    #     gray = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+    #
+    #     gray = cv2.GaussianBlur(gray, (11, 11), 0)
+    #     ret, gray = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+    #
+    #
+    #
+    #     white_coords = np.argwhere(mask == 255)
+    #
+    #     if white_coords.shape == (0, 2):
+    #         topmost_last = (360, 512)
+    #     else:
+    #         topmost_last = np.average(white_coords, axis=0)
+    #         topmost_last = topmost_last.astype(np.int)
+    #         topmost_last[0], topmost_last[1] = topmost_last[1], topmost_last[0]
+    #         topmost_last = tuple(topmost_last)
+    #
+    #     cv2.circle(img, topmost_last, 2, (0, 255, 0), -1)
+    #
+    #     cv2.imshow('Gray', mask)
+    #     cv2.imshow('Img', img)
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         break
+
+
